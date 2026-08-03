3ee7d9c79798c191f61b9a8052531692329b09da
abhay patel <abhayjipatel9821@gmail.com>
2026-08-03 15:13:24 +0530

    initial commit

--- train_model.py
+++ train_model.py
@@ -4,21 +4,19 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import r2_score
 
-# load dataset
+
 df = pd.read_csv("kc_house_data.csv")
 
-# select features
 df = df[['sqft_living', 'bedrooms', 'bathrooms', 'floors', 'waterfront', 'grade', 'price']]
 
-# features & target
 X = df[['sqft_living', 'bedrooms', 'bathrooms', 'floors', 'waterfront', 'grade']]
 y = df['price']
 
-# split
+
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
-# model
+
 model = RandomForestRegressor(
     n_estimators=50,
     max_depth=15,
@@ -28,11 +26,11 @@ model = RandomForestRegressor(
 
 model.fit(X_train, y_train)
 
-# evaluation
+
 y_pred = model.predict(X_test)
 print("R2 Score:", r2_score(y_test, y_pred))
 
-# save
+
 joblib.dump(model,"model.pkl",compress=3)
 
 print("✅ Model trained!")
